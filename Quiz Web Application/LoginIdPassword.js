7985fdab987196ac7224f4c07242ef8002c78976
sakshibansal30 <80977219+sakshibansal30@users.noreply.github.com>
2023-04-10 04:33:42 +0530

    Add files via upload

---
+++ Quiz Web Application/LoginIdPassword.js
@@ -0,0 +1,15 @@
+// creating an array and passing the number, questions, options, and answers
+let loginIdPassword = [
+    {
+    LoginId: "Sakshi@gmail.com",
+    Password: "password123"
+  },
+  {
+    LoginId:"Meenakshi@gmail.com",
+    Password: "password123"
+  },
+  {
+    LoginId: "pankaj@gmail.com",
+    Password: "password123"
+  }
+    ];
---
+++ Quiz Web Application/questions.js
@@ -0,0 +1,170 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A metallic sphere has a cavity of diameter D at its center.If the sphere is heated,the diameter of the cavity will",
+    answer: "Increase",
+    options: [
+      "Decrease",
+      "Increase",
+      "Remain unchanged",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which solid will have the weakest intermolecular forces?",
+    answer: "Ice",
+    options: [
+      "Ice",
+      "Phosphorus",
+      "Naphthalene",
+      "Sodium fluoride"
+    ]
+  },
+  {
+    numb: 3,
+    question: "In how many ways can a team of 11 players be formed out of 25 players, if 6 out of them are always to be included and 5 always to be excluded",
+    answer: "2002",
+    options: [
+      "2020",
+      "2002",
+      "2008",
+      "8002"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of these is not true for adiabatic process ?",
+    answer: "dT=0",
+    options: [
+      "dQ=0",
+      "dU+dW=0",
+      "dT=0",
+      "Molar specific heat is zero"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The tendency of a reaction to take place spontaneously is greatest when the reaction is",
+    answer: "Exothermic and randomness increases",
+    options: [
+      "Exothermic and randomness increases",
+      "Endothermic & randomness increases",
+      "Exothermic and randomness decreases",
+      "Endothermic & randomness decreases"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which of these is inertial frame of reference?",
+    answer: "A observer in cycle moving with constant velocity",
+    options: [
+      "A observer in car slowing down",
+      "A observer in train speeding up",
+      "A observer in car in uniform circular motion",
+      "A observer in cycle moving with constant velocity"
+    ]
+  },
+  {
+    numb: 7,
+    question: "A fair die is tossed repeatedly. A wins if it is 1 or 2 on two consecutive tosses and B wins if it is 3, 4, 5 or 6 on two consecutive tosses. The probability that A wins if the die is tossed indefinitely is",
+    answer: "5/21",
+    options: [
+      "1/3",
+      "5/21",
+      "1/4 ",
+      "2/5"
+    ]
+  },
+  {
+    numb: 8,
+    question: "For the four successive transition elements (Cr, Mn, Fe and Co), the stability of+2 oxidation state will be there in which of the following order?",
+    answer: "Mn > Cr > Fe > Co",
+    options: [
+      "Fe > Mn > Co > Cr",
+      "Cr > Mn > Co > Fe",
+      "Mn > Cr > Fe > Co",
+      "Co > Mn > Fe > Cr"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Television signals on earth cannot be received at distances greater than 100 km from the transmission station. Thereason behind this is that",
+    answer: "The surface of earth is curved like a sphere",
+    options: [
+      "The receiver antenna is unable to detect the signal at a distance greater than 100 km",
+      "The TV programme consists of both audio and video signals",
+      "The TV signals are less powerful than radi signals",
+      "The surface of earth is curved like a sphere"
+    ]
+  },
+  {
+    numb: 10,
+    question: "If A = {1, 2, 3, 4, 5} and B = {2, 3, 6, 7} then the number of elements in (A×B)∩(B×A) is",
+    answer: "4",
+    options: [
+      "20",
+      "18",
+      "6",
+      "4"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which of the following statement is not true about the hexagonal close packing?",
+    answer: "In this arrangement, spheres of the fourth layer are exactly aligned with those of the first layer",
+    options: [
+      "The coordination number is 12",
+      "It has 74% packing efficiency",
+      "Tetrahedral voids of the second layer are covered by the spheres of the third layer",
+      "In this arrangement, spheres of the fourth layer are exactly aligned with those of the first layer"
+    ]
+  },
+  {
+    numb: 12,
+    question: "A small rod of bismuth is suspended freely between the poles of a strong electromagnet. It is found to arrange itself at right angles to the magnetic field. This observation establishes that bismuth is",
+    answer: "Diamagnetic",
+    options: [
+      "Diamagnetic",
+      "Paramagnetic",
+      "Fern-magnetic",
+      "Antiferro-magnetic"
+    ]
+  },
+  {
+    numb: 13,
+    question: "If p is any statement, then which of the following is a tautology?",
+    answer: "p ∨ (~p)",
+    options: [
+      "p ∧ f",
+      "p ∨ f",
+      "p ∨ (~p)",
+      "p ∧ t"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Out of the following which has highest dipole moment?",
+    answer: "cis-3-hexene",
+    options: [
+      "2,2-dimethyl propane",
+      "trans-2-pentene",
+      "cis-3-hexene",
+      "2,2,3,3-tetramethyl butane"
+    ]
+  },
+  {
+    numb: 15,
+    question: "A sheet of aluminium foil of negligible thickness is introduced between the plates of a capacitor. The capacitance of the capacitor",
+    answer: "Remains unchanged",
+    options: [
+      "Decreases",
+      "Remains unchanged",
+      "Becomes infinite",
+      "Increases"
+    ]
+  },
+
+
+];
